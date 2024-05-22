d91c78b86e06aee10104dc648fb0caedcf1bda42
Terry Sun <terry_sun@htc.com>
2024-05-22 13:40:05 +0800

    first version

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_KlocworkTest;
+
+Console.WriteLine("Hello, World!");
+
+if (null == Service.TestInstance)
+{
+    return;
+}
+List<string> testList = null;
+Console.WriteLine($"testListLength: {testList.Count}");
+Object myObj = null;
+string s = (myObj ?? String.Empty).ToString();
+Console.WriteLine("Time"+ Service.TestInstance.ToString());
---
+++ Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_KlocworkTest
+{
+    internal class Service
+    {
+        public static DateTime TestInstance;
+        public Service()
+        {
+            if(TestInstance == null)
+            {
+                TestInstance = DateTime.Now;
+            }
+        }
+       
+    }
+
+}
